cf35cda120a9d4a25cbc3cd962fb83df9d2d017f
dong4j <dong4j@gmail.com>
2025-06-29 16:45:52 +0800

    refactor(cubo-starter-examples): 调整示例项目的配置和代码结构

--- templates/cubo-{{name}}-spring-boot-sample/cubo-{{name}}-spring-boot-sample-{{module}}/src/main/java/dev/dong4j/zeka/starter/sample/{{name}}/Sample{{name}}Application.java
+++ templates/cubo-{{name}}-spring-boot-sample/cubo-{{name}}-spring-boot-sample-{{module}}/src/main/java/dev/dong4j/zeka/starter/sample/{{name}}/Sample{{name}}Application.java
@@ -1,14 +1,12 @@
 package dev.dong4j.zeka.starter.sample.{{name}};
 
 import dev.dong4j.zeka.starter.launcher.ZekaStarter;
-import dev.dong4j.zeka.starter.launcher.annotation.RunningType;
-import dev.dong4j.zeka.starter.launcher.enums.ApplicationType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 示例启动类
- * 
+ *
  * @author dong4j
  * @version 1.0.0
  * @email "mailto:dong4j@gmail.com"
@@ -16,7 +14,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0.0
  */
 @Slf4j
-@RunningType(ApplicationType.SERVICE)
 @SpringBootApplication
 public class Sample{{Name}}Application extends ZekaStarter {
 }
