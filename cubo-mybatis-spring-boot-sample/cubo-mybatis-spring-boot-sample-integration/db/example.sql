bd57ddbb1fc00e1c9fc5d442580b35761a01002c
dong4j <dong4j@gmail.com>
2025-10-07 14:02:51 +0800

    refactor(db): 统一时间字段类型为datetime
    - 将所有表中的create_time和update_time字段类型从timestamp改为datetime

--- cubo-mybatis-spring-boot-sample/cubo-mybatis-spring-boot-sample-integration/db/example.sql
+++ cubo-mybatis-spring-boot-sample/cubo-mybatis-spring-boot-sample-integration/db/example.sql
@@ -5,20 +5,20 @@ drop table if exists mybatis_spring_boot_sample_integration.`user`;
 create table if not exists mybatis_spring_boot_sample_integration.`user`
 (
     `id`           bigint unsigned auto_increment comment '自增主键' primary key,
-    `phone`        varchar(11)                         not null comment '用户电话号码',
-    `user_name`    varchar(64)                         not null comment '用户名',
-    `gender`       tinyint(4)                          not null comment '性别',
-    `nick_name`    varchar(64)                         not null comment '姓名',
-    `company_name` varchar(64)                         not null comment '公司名称',
-    `password`     varchar(128)                        not null comment '密码',
-    `salt`         varchar(128)                        not null comment '密码加密盐值',
-    `api_key`      varchar(50)                         null comment 'open api key',
-    `secret_key`   varchar(50)                         null comment 'open api secret key',
-    `email`        varchar(128)                        null comment '用户邮箱',
-    `state`   tinyint(4) unsigned not null default 0 comment '用户状态:0: 未审核, 1: 审核中, 2: 审核未通过, 3: 已锁定, 4: 正常',
-    `deleted` bigint default 0    not null comment '是否删除: 0-未删除, 大于 1 为已删除',
-    `create_time`  timestamp default current_timestamp not null comment '创建时间 (公共字段)',
-    `update_time`  timestamp default current_timestamp not null comment '最后更新时间 (公共字段)'
+    `phone`        varchar(11)                        not null comment '用户电话号码',
+    `user_name`    varchar(64)                        not null comment '用户名',
+    `gender`       tinyint(4)                         not null comment '性别',
+    `nick_name`    varchar(64)                        not null comment '姓名',
+    `company_name` varchar(64)                        not null comment '公司名称',
+    `password`     varchar(128)                       not null comment '密码',
+    `salt`         varchar(128)                       not null comment '密码加密盐值',
+    `api_key`      varchar(50)                        null comment 'open api key',
+    `secret_key`   varchar(50)                        null comment 'open api secret key',
+    `email`        varchar(128)                       null comment '用户邮箱',
+    `state`        tinyint(4) unsigned                not null default 0 comment '用户状态:0: 未审核, 1: 审核中, 2: 审核未通过, 3: 已锁定, 4: 正常',
+    `deleted`      bigint   default 0                 not null comment '是否删除: 0-未删除, 大于 1 为已删除',
+    `create_time`  datetime default current_timestamp not null comment '创建时间 (公共字段)',
+    `update_time`  datetime default current_timestamp not null comment '最后更新时间 (公共字段)'
 ) comment '用户信息表' engine = InnoDB
                        charset = utf8mb4
                        collate = utf8mb4_general_ci;
@@ -37,16 +37,16 @@ values (1, '18000000000', 'dong4j', 1, 'dong4j', 'iHome', '000000', '000000', '0
 create table mybatis_spring_boot_sample_integration.rule
 (
     id            bigint auto_increment primary key,
-    ip_from       bigint                              null comment 'IP范围开始地址',
-    ip_to         bigint                              null comment 'IP范围结束地址',
-    match_mode    varchar(20)                         null comment '域名匹配模式',
-    name          varchar(100)                        null comment '匹配的域名',
-    priority      int                                 null comment '匹配优先级',
-    enabled       bit(1)    default b'1'              null comment '是否启用',
-    dispatch_mode varchar(20)                         null comment '分发模式, 如iphash、round-robin、random',
-    deleted bigint default 0 not null comment '是否删除: 0-未删除, 大于 1 为已删除',
-    create_time   timestamp default current_timestamp not null comment '创建时间 (公共字段)',
-    update_time   timestamp default current_timestamp not null comment '最后更新时间 (公共字段)'
+    ip_from       bigint                             null comment 'IP范围开始地址',
+    ip_to         bigint                             null comment 'IP范围结束地址',
+    match_mode    varchar(20)                        null comment '域名匹配模式',
+    name          varchar(100)                       null comment '匹配的域名',
+    priority      int                                null comment '匹配优先级',
+    enabled       bit(1)   default b'1'              null comment '是否启用',
+    dispatch_mode varchar(20)                        null comment '分发模式, 如iphash、round-robin、random',
+    deleted       bigint   default 0                 not null comment '是否删除: 0-未删除, 大于 1 为已删除',
+    create_time   datetime default current_timestamp not null comment '创建时间 (公共字段)',
+    update_time   datetime default current_timestamp not null comment '最后更新时间 (公共字段)'
 ) comment '解析规则' charset = utf8mb4
                      collate = utf8mb4_general_ci;
 
